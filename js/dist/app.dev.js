9c288f1bca244c8aa6ba37e33e8bb474e106c836
MohammedJahaf <mohdjahaf201777@gmail.com>
2020-09-18 19:28:40 +0300

    first commit

---
+++ js/dist/app.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(window).scroll(function () {
+  $(window).scrollTop() > 50 ? $('.main-nav').addClass('fixed') : $('.main-nav').removeClass('fixed');
+});
+$(document).ready(function () {
+  $(document).on('click', function () {
+    $('.custom-collapse').removeClass('show');
+  });
+});
